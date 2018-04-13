c4bff9b5e3b22562cd171f2e23fd5e460fb0c06c
Pranav Mehendiratta <mehendiratta@rockhopper-03.cs.wisc.edu>
2018-04-12 19:23:43 -0500

    8 tests passing (new - threads_zombies)

--- kernel/proc.h
+++ kernel/proc.h
@@ -75,6 +75,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   void* stack;
+  void* tc;
 };
 
 // Process memory is laid out contiguously, low addresses first:
